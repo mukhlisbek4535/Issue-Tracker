55141130b5c787d068960886a0d2650cc6c23c85
Mukhlisbek <mukhlisbekkirgizov@gmail.com>
2026-01-26 16:13:45 +0500

    fixed route

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: 'https://issue-tracker-seven-gamma.vercel.app/login',
+    origin: 'https://issue-tracker-seven-gamma.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
